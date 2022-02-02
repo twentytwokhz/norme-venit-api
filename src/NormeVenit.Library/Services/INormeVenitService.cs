91f2fffc7c511e6d70a4e4008076a6f6fdf6ca57
Florin Bobiș <florin.bobis@tecknoworks.com>
2022-02-02 22:57:35 +0200

    dsasa

--- src/NormeVenit.Library/Services/INormeVenitService.cs
+++ src/NormeVenit.Library/Services/INormeVenitService.cs
@@ -6,6 +6,13 @@ namespace NormeVenit.Library.Services
 {
     public interface INormeVenitService
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="caen"></param>
+        /// <param name="judet"></param>
+        /// <param name="codActivitate"></param>
+        /// <returns></returns>
         List<NormaVenit> GetNormaVenit(string caen, string judet, string codActivitate);
         int GetNormaVenit(string caen, string judet, string codActivitate, TipLocalitate tipLocalitate);
     }
