cb9d165b2b3013c1858437054a7f6b26f8849a5e
Florin Bobiș <florin.bobis@tecknoworks.com>
2022-02-02 22:54:20 +0200

    test

--- src/NormeVenit.Library/Services/INormeVenitService.cs
+++ src/NormeVenit.Library/Services/INormeVenitService.cs
@@ -6,7 +6,22 @@ namespace NormeVenit.Library.Services
 {
     public interface INormeVenitService
     {
+        /// <summary>
+        /// Aduce normele de venit per activitate
+        /// </summary>
+        /// <param name="caen"></param>
+        /// <param name="judet"></param>
+        /// <param name="codActivitate"></param>
+        /// <returns></returns>
         List<NormaVenit> GetNormaVenit(string caen, string judet, string codActivitate);
+        /// <summary>
+        /// Aduce norma de venit specifica tipului de localitate
+        /// </summary>
+        /// <param name="caen"></param>
+        /// <param name="judet"></param>
+        /// <param name="codActivitate"></param>
+        /// <param name="tipLocalitate"></param>
+        /// <returns></returns>
         int GetNormaVenit(string caen, string judet, string codActivitate, TipLocalitate tipLocalitate);
     }
 }
